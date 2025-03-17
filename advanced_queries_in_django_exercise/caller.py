27e091dd1acab51ff66ab11774b561df16d59f5b
mariami57 <mmitova3@gmail.com>
2025-03-17 13:03:13 +0200

    added more solutions

---
+++ advanced_queries_in_django_exercise/caller.py
@@ -0,0 +1,16 @@
+import os
+import django
+
+
+# Set up Django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
+django.setup()
+
+# Import your models here
+from main_app.models import RealEstateListing, VideoGame, BillingInfo, Invoice, Technology, Project, Programmer, \
+    Exercise
+
+# Run and print your queries
+
+
+
